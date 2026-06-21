03d25de7a59abaf659d168efc9a23ea649aaf0ff
koudjo02 <samuel.adzogble@epitech.eu>
2026-06-21 23:32:38 +0000

    Fix définitif: retrait de @vueuse/nuxt incompatible

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-const vehiculesVente = [
-  { id: 1, title: 'Audi RS7', price: '40 000 000', year: 2022, km: '35 000', image: '/images/vehicules/rs7.jpg', type: 'vente' }
-]
+const message = 'Bonjour'
 </script>
 
 <template>
   <div class="p-10">
-    <h1 class="text-3xl font-bold">Test sans import</h1>
-    <p>{{ vehiculesVente.length }} véhicule(s)</p>
+    <h1>{{ message }}</h1>
   </div>
 </template>
