0304180f47f518e55b279f0acd46fc4015fb0d99
koudjo02 <samuel.adzogble@epitech.eu>
2026-06-21 23:07:50 +0000

    Test ultime: aucun module, config ttt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/icon','@pinia/nuxt'],
+  modules: ['@nuxt/ui', '@nuxt/icon','@pinia/nuxt','@vueuse/nuxt'],
   css: ['~/assets/css/main.css'],
   
 })
