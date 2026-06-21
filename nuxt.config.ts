722beb801cc4c8b5286593b63773076db217c15b
koudjo02 <samuel.adzogble@epitech.eu>
2026-06-21 22:58:18 +0000

    Test ultime: aucun module, config minimale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css']
 })
